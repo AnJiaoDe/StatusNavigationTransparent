ef051f4186c3f49cecf9e56724c1a01d7a05c8ba
437042721@qq.com <437042721@qq.com>
2020-08-12 12:22:13 +0800

    更新到androidx

--- app/src/main/java/com/cy/statusnavigationtrans/BaseActivity.java
+++ app/src/main/java/com/cy/statusnavigationtrans/BaseActivity.java
@@ -2,9 +2,10 @@ package com.cy.statusnavigationtrans;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.cy.translucentparent.StatusNavUtils;
 
